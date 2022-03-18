d0327f6ba172f8a5cfda6655da06f8202a7c0404
D1SS <D1SS@DESKTOP-2QR7GQG>
2022-03-18 18:36:04 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isDoorOpen;
+            int health;
+            long amountOfGold;
+            string personName;
+            float timeBeforeExit;
+            double armor;
+            byte rewardStars;
+            ulong starsInTheSky;
+            bool canJump;
+            int bulletsInGun;
+                              
+
+        }
+    }
+}
